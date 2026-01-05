1ea0d6a79fa35e01df8e8ead607d79e4b6d89604
Garihc <chiragramanna@gmail.com>
2026-01-05 17:27:23 +0530

    feat(frontend): Implement update todo method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { MdOutlineDone } from "react-icons/md";
+import { MdOutlineDone, MdModeEditOutline } from "react-icons/md";
+import { FaTrash } from "react-icons/fa6"
+import { IoClose } from "react-icons/io5"
 
 function App() {
 
   const [description, setDescription] = useState("");
   const [todos, setTodos] = useState([]);
-  const [editTodo, setEditTodo] = useState(null);
-  const [editedText, useEditedText] = useState("");
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [editedText, setEditedText] = useState("");
 
   //Functionality to get all Todos
   const getTodos = async() => {
@@ -34,6 +36,19 @@ function App() {
       });
       setDescription("");
       getTodos(); //so that a todo is visible as soon as form is submitted
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const saveEdit = async (id) => {
+    try{
+      await axios.put(`http://localhost:5000/todos/${id}`, {
+        description: editedText,
+      });
+      setEditingTodo(null);
+      setEditedText("");
+      getTodos();
     } catch (err) {
       console.error(err.message);
     }
@@ -68,11 +83,50 @@ function App() {
           ) : (
             <div className="flex flex-col gap-y-4">
               {todos.map((todo) => ( //iterator for all todos
-                <div className="flex items-center gap-x-4">
-                  <button className={`h-6 w-6 border-2 rounded-full flex items-center justify-center ${todo.completed ? "bg-green-500 border-green-500 text-white" : "border-gray-300 hover:border-blue-400"}`}>
-                    {todo.completed && <MdOutlineDone size={16} />}
-                  </button>
-                  <span>{todo.description}</span>
+                <div key={todo.todo_id} className="pb-4">
+                  {editingTodo === todo.todo_id ? (
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        className="flex-1 p-3 border rounded-lg border-gray-200 outline-none focus:ring-2 focus:ring-blue-300 text-gray-700 shadow-inner" type="text"
+                        value={editedText}
+                        onChange={(e) => setEditedText(e.target.value)} 
+                      />
+                      <div>
+                        <button
+                          onClick={() => saveEdit(todo.todo_id)}
+                          className="px-4 py-2 bg-green-500 text-white rounded-lg mr-2 mt-2 hover:bg-green-600 duration-200"
+                        >
+                          <MdOutlineDone />
+                        </button>
+                        <button
+                          onClick={() => setEditingTodo(null)}
+                          className="px-4 py-2 bg-gray-500 text-white rounded-lg mt-2 hover:bg-gray-600 duration-200"
+                        >
+                          <IoClose />
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex justify-between items-center">
+                      <div className="flex items-center gap-x-4">
+                        <button className={`h-6 w-6 border-2 rounded-full flex items-center justify-center ${todo.completed ? "bg-green-500 border-green-500 text-white" : "border-gray-300 hover:border-blue-400"}`}>
+                          {todo.completed && <MdOutlineDone size={16} />}
+                        </button>
+                        <span>{todo.description}</span>
+                      </div>
+                      <div className="flex gap-x-2">
+                          <button onClick={() => {
+                            setEditingTodo(todo.todo_id);
+                            setEditedText(todo.description);
+                          }} className="p-2 text-blue-500 hover:text-blue-700 rounded-lg hover:bg-blue-50 duration-200">
+                            <MdModeEditOutline />
+                          </button>
+                          <button className="p-2 text-red-500 hover:text-red-700 rounded-lg hover:bg-red-50 duration-200">
+                            <FaTrash />
+                          </button>
+                        </div>
+                    </div>
+                  )}
                 </div>
               ))}
             </div>
